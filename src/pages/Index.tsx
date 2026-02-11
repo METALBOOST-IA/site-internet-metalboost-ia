164cb7abde2e3192f8c7cd62d30d322aee3884d9
METALBOOST-IA <gregory.rota@metalboost-ia.fr>
2026-02-11 13:09:33 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,3 @@
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MM3SW794');</script>
-<!-- End Google Tag Manager -->import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import Layout from "@/components/Layout";
 import { ArrowRight, Target, Zap, TrendingUp, Clock, Shield, CheckCircle2, Factory, Cog, BarChart3, Users } from "lucide-react";
