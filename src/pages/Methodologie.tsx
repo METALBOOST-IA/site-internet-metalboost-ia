9fa85537f187767f16d6f031a6e81181342e8ab1
METALBOOST-IA <gregory.rota@metalboost-ia.fr>
2026-02-11 12:58:38 +0100

    Update Methodologie.tsx

--- src/pages/Methodologie.tsx
+++ src/pages/Methodologie.tsx
@@ -1,4 +1,10 @@
-import { Link } from "react-router-dom";
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MM3SW794');</script>
+<!-- End Google Tag Manager -->import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import Layout from "@/components/Layout";
 import { ArrowRight, Search, Target, Wrench, BarChart3, CheckCircle2, Clock, Users, Lightbulb, Rocket } from "lucide-react";
